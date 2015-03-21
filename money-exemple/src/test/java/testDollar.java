cabe5898b0f563de3200def37f7a999c1ee5dc50
oscar-raig <oscar.raig@gmail.com>
2015-03-21 22:40:02 +0100

    Adding Test

--- money-exemple/src/test/java/testDollar.java
+++ money-exemple/src/test/java/testDollar.java
@@ -12,7 +12,7 @@ public class testDollar {
     Assert.assertTrue(new Dollar(5).equals(new Dollar(5)));
     Assert.assertFalse(new Dollar(5).equals(new Dollar(6)));
   }
-
+  @Test
   public void testDollarstep4() {
     Dollar five= new Dollar(5);
     Assert.assertEquals(new Dollar(10), five.times(2));
