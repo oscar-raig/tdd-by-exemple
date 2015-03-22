4837ffce4a8aa4a3a3e8cafdbd4d1d5787bcb450
oscar-raig <oscar.raig@gmail.com>
2015-03-22 20:12:30 +0100

    making Pair private

--- money-exemple/src/main/java/Bank.java
+++ money-exemple/src/main/java/Bank.java
@@ -8,6 +8,24 @@ import java.util.Hashtable;
  */
 public class Bank {
 
+  private class Pair {
+
+    private String from;
+    private String to;
+
+    Pair(String from, String to) {
+      this.from = from;
+      this.to = to;
+    }
+    public boolean equals(Object object) {
+      Pair pair = (Pair) object;
+      return from.equals(pair.from) & to.equals(pair.to);
+    }
+    public int hashCode() {
+      return 0;
+    }
+  }
+
   private Hashtable rates= new Hashtable();
 
   public Money reduce(Expression source, String to) {
